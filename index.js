3efd57ffc4e47c958eb8db4d651b3074b6b0fedb
sandraNotTaken <155741111+sandraNotTaken@users.noreply.github.com>
2026-01-14 18:06:46 +0100

    new changes

--- index.js
+++ index.js
@@ -4,13 +4,27 @@ const getSchemeBtn = document.getElementById("getSchemeBtn");
 const colorDisplayArea = document.getElementById("colorDisplayArea");
 const colorDisplay = document.getElementById("color-display");
 
+fetchInitialColors()
+
 getSchemeBtn.addEventListener("click", () => {
+    fetchInitialColors();
+}).catch(err => console.log('Error fetching color scheme:', err));
+
+function fetchInitialColors() {
     const colorPicked = colorPicker.value.slice(1);
     const schemePicked = schemeType.value;
 
     fetch(`https://www.thecolorapi.com/scheme?hex=${colorPicked}&mode=${schemePicked}&count=5`)
     .then(res => res.json())
     .then(data => {
-        
+        colorDisplayArea.innerHTML = "";
+        data.colors.forEach(color => {
+            colorDisplayArea.innerHTML += `
+                <div class="color-item">
+                    <div class="color-box" style="background-color: ${color.hex.value};"></div>
+                    <p class="color-code">${color.hex.value}</p>
+                </div>
+            `;
+        });
     })
-})
+}
